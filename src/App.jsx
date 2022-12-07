642e1ec9513b4a9b1311445421e6d5b3050f31e6
coddedwells <wells.mail.13@gmail.com>
2022-12-07 19:07:14 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Jokes from "./components/Jokes";
+import "./App.css"
+
+const App = () => {
+  return (
+    <div className="App">
+		<Jokes/>
+    </div>
+  );
+};
+
+export default App;
